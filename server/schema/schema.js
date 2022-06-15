03d76da7b1878ddb5b9b39d6f41936cbfd57a163
Noam Meir <93981788+meirson1@users.noreply.github.com>
2022-06-15 20:18:51 +0300

    Client queries

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,16 @@
-const { projects, clients } = require("../sampleData");
+// Mongoose models
+const Project = require("../models/Project");
+const Client = require("../models/Client");
 
-const { GraphQLObjectType, GraphQLID, GraphQLString } = require("graphql");
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLList,
+} = require("graphql");
 
+//Client type
 const ClientType = new GraphQLObjectType({
   name: "Client",
   fields: () => ({
@@ -12,18 +21,63 @@ const ClientType = new GraphQLObjectType({
   }),
 });
 
+//Project type
+const ProjectType = new GraphQLObjectType({
+  name: "Projects",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+    client: {
+      type: ClientType,
+      resolve(parent, args) {
+        return Client.findById(parent.clientId);
+      },
+    },
+  }),
+});
+
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
 
   fields: {
+    //All Projects
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parent, args) {
+        return Project.find();
+      },
+    },
+
+    //Project by ID
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return Project.findById(args.id);
+      },
+    },
+
+    //All Clients
+    clients: {
+      type: new GraphQLList(ClientType),
+      resolve(parent, args) {
+        return Client.find();
+      },
+    },
+
+    //Client by ID
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return;
+        return Client.findById(args.id);
       },
     },
   },
 });
 
-const ProjectType = new GraphQLObjectType({});
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
