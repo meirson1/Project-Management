3062c1ab5d206535f8729c8379de146f042e98df
Noam Meir <93981788+meirson1@users.noreply.github.com>
2022-06-16 23:55:28 +0300

    Display Projects

---
+++ client/src/App.js
@@ -0,0 +1,51 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Project from "./pages/Project";
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache,
+});
+
+function App() {
+  return (
+    <>
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="*" element={<NotFound />}></Route>
+              <Route path="/projects/:id" element={<Project />}></Route>
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </ApolloProvider>
+    </>
+  );
+}
+
+export default App;
